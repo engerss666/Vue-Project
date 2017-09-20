7dfb27237874aebf96dae8711ee62e29f58d5c0c
engerss <837049530@qq.com>
2017-09-20 15:37:16 +0800

    头部导航

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/Home'
+import Home from '@/components/home/Home'
+// import Login from '@/components/login/Login'
 
 Vue.use(Router)
+
+// const login = r => require.ensure([], () => r(require('@/components/login/login')), 'login');
+// const home = r => require.ensure([], () => r(require('@/components/home/Home')), 'login');
 
 export default new Router({
   routes: [
