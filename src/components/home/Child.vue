b7b85d7dd97438c97cc9a7846e848b9469554291
engerss <837049530@qq.com>
2017-09-26 17:34:41 +0800

    0926

---
+++ src/components/home/Child.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h3>子组件</h3>
+        <p>{{message}}</p>
+    </div>
+</template>
+<script>
+export default {
+        props: ['message']
+}
+</script>
+<style>
+
+</style>
+
